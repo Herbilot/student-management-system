be66676e92f4bd9f8a0acd7f1c9665ea601a5ae9
herbilot <hermaregie@gmail.com>
2023-01-10 13:53:46 +0000

    merging the two branches

--- src/main/java/tech/herbidev/subjectgroup/models/Groups.java
+++ src/main/java/tech/herbidev/subjectgroup/models/Groups.java
@@ -1,6 +1,43 @@
 package tech.herbidev.subjectgroup.models;
 
+import java.util.List;
+
 public class Groups {
-	private String name;
+	private int number;
+	private String name = "Groupe";
+	private List<Students> member;
+	
+	public Groups( ) {}
+
+	public Groups(int number, String name, List<Students> member) {
+		this.number = number;
+		name = this.name;
+		this.member = member;
+	}
+
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Students> getMember() {
+		return member;
+	}
+
+	public void setMember(List<Students> member) {
+		this.member = member;
+	}
 	
 }
